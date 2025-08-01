3f76aea708b064b0ac60f10db81ebe43458f5584
coby <coby0228@gmail.com>
2025-08-01 12:05:48 +0800

    update label

--- process_data.py
+++ process_data.py
@@ -6,16 +6,23 @@ input_filename = 'data/raw/TFC_migration_data.json'
 output_filename = 'data/dataset/TFC_data_with_labels.json'
 
 
+def map_function(verdict):
+    """
+    Maps the verdict to a label ('false' or 'half').
+    """
+    if verdict in ['錯誤', '詐騙', '移花接木', '假借冠名', '影像變造', '影音變造']:
+        return 'false'
+    else:
+        return 'half'
+
+
 with open(input_filename, 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 if isinstance(data, list):
     for item in data:
-        if 'verdict' in item:
-            if item['verdict'] == '錯誤':
-                item['label'] = 'false'
-            else:
-                item['label'] = 'half'
+        if 'verdict' in item and item['verdict']:
+            item['label'] = map_function(item['verdict'])
         else:
             item['label'] = 'half'
         
